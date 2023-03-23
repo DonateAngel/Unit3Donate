560210788f4ba4c35631651977d25cd853db48fb
DonateAngel <705025@student.dusd.net>
2023-03-23 09:55:54 -0700

    Started on Animatons
    Started on animations and fixed gravity

--- Unit3DonateP1/Assets/Course Library/Scipts/PlayerControl.cs
+++ Unit3DonateP1/Assets/Course Library/Scipts/PlayerControl.cs
@@ -8,10 +8,13 @@ public class PlayerControl : MonoBehaviour
     public float jumpForce;
     public float gravityModifier;
     public bool isOnGround = true;
+    public bool gameOver = false;
+    private Animator playerAnim;
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
         
     }
@@ -19,16 +22,24 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
-            
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)  
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true;
+       if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+       else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+            Debug.Log("GAME OVER!");
+        }
     }
 }
